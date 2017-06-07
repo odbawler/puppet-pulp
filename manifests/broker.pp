5a3fa07a33a338e4faaf10798b228ca96db468e5
odbawler <Oliver.Bawler@landregistry.gov.uk>
2017-06-07 10:04:57 +0100

    puppet changes

--- manifests/broker.pp
+++ manifests/broker.pp
@@ -1,13 +1,25 @@
 # Set up the broker
 class pulp::broker {
 
-  $broker_service = 'rabbitmq-server'
-
-  if $pulp::manage_broker {
-    include ::rabbitmq
+  if $pulp::messaging_transport == 'qpid' {
+    $broker_service = 'qpidd'
+  } elsif $pulp::messaging_transport == 'rabbitmq' {
+    $broker_service = 'rabbitmq-server'
   }
 
-  Class['pulp::service']
+  if $pulp::manage_broker {
+    if $pulp::messaging_transport == 'qpid' {
+      include ::qpid
+    } elsif $pulp::messaging_transport == 'rabbitmq' {
+      include ::rabbitmq
+    }
+  } else {
+    if $pulp::messaging_transport == 'qpid' {
+      include ::qpid::tools
+
+      Class['qpid::tools'] -> Class['pulp::service']
+    }
+  }
 
   Service <| title == $broker_service |> -> Class['pulp::service']
   Service <| title == $broker_service |> -> Exec['migrate_pulp_db']
